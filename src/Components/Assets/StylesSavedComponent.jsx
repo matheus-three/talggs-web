bc2fd819d657045b67214ffc6dbf304a32792449
Sthéffane Nunes <stheffane.tr@gmail.com>
2020-04-20 01:07:03 -0300

    Alterado estilo StyleSavedComponent.jsx, trocado tema da navbar

--- src/Components/Assets/StylesSavedComponent.jsx
+++ src/Components/Assets/StylesSavedComponent.jsx
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body{
-    background: #f7f6ee;
+    background: #F7F6EE;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
 	font-family: Helvetica, sans-serif;
@@ -25,7 +25,7 @@ export const HeaderStyle = styled.div`
   height: 11vh;
   width: 100vw;
   font-size: 1.6vw;
-  background-color: #2D9AA6;
+  background-color: #101D25;
   display:flex;
   align-items: center;
     
@@ -38,7 +38,7 @@ export const HeaderStyle = styled.div`
   ul {
     display: flex;
     width: 50vw;
-    color: #f7f6ee;
+    color: #F7F6EE;
     justify-content: space-evenly;
 	list-style: none;
 	margin: auto;
@@ -113,15 +113,15 @@ export const IconEdit = styled.div`
 
 //Estilo que engloba os relatórios salvos da tela .
 export const MainStyle = styled.div`
-    display:flex;
+    display: flex;
     flex-wrap: wrap;
-    margin-top:50px; 
+	margin-top: 50px;
   	
 		span {
 			margin-left: 15%;
 			font-size: 3em;
 			margin-top: 50px;
-			color: #ddd;
+			color: #bebcbc7a;
 		}
 
 	@media only screen and (max-width: 600px) {
@@ -131,11 +131,13 @@ export const MainStyle = styled.div`
 `
 //Posicionamento do Titulo dos relatórios
 export const TitleMainStyle = styled.div`
-  width: 350px;
   height:auto;
-  margin-top: 100px;
-  margin-left: 60px;
-  font-size: 2em;    
+  margin-top: 10vh;
+  margin-left: 6vw;
+  font-size: 3.5em;
+  color: #F2A950;
+  letter-spacing: 2px;
+  font-weight: bold;    
 
 	@media only screen and (max-width: 600px) {
 	  margin-top: 30px;
@@ -143,20 +145,23 @@ export const TitleMainStyle = styled.div`
 `
 //Estilo de cada relatório salvo.
 export const SaveReportsStyle = styled.button`
-  background-color:#232f40;
-  width:300px;
-  height: 100px;
-  margin: 0px 60px;
-	margin-left:100px;
-  border-radius:25px;
-  display:flex;
-  align-items:center;
-  justify-content: center;
-  margin-bottom:50px;
-  border:none;  
-	color:#f7f6ee;
+	background-color: #232f40;
+	width: 22vw;
+	height: 13vh;
+	color: #BEBCBC;
+	font-size: 2.2em;
+	border: none;
+	border-radius: 25px;
+	box-shadow: 5px 5px 17px #808080a6;
+	padding: 15px 25px;
+	margin-left: 130px;
+    margin-bottom: 100px;
 	cursor: pointer;
-
+	display: flex;
+	align-items: flex-start;
+	-webkit-box-pack: start;
+	justify-content: left;
+	
 
 	@media only screen and (max-width: 1200px) {
 		width:180px;
@@ -173,46 +178,60 @@ export const SaveReportsStyle = styled.button`
 `
 //Posicionamento do Botão de criar relatórios.
 export const ButtonCreateStyle = styled.div`
-  display:flex;
-  justify-content: flex-end;
-	align-items:flex-end;
-  margin-right: 100px;
-  margin-top: 30px;
-  margin-bottom:30px;
-	padding:20px;
+display: flex;
+align-items: center;
+position: fixed;
+bottom: 50px;
+right: 60px;
+transition: .3s all;
 
-  span{
-      font-size:1.3em;
-      margin:10px;
-      display:flex;
-      align-items:center;
-  }
+	&:hover span {
+		visibility: visible;
+	}
+
+	span {
+      font-size: 1.7em;
+      margin: 10px;
+      display: flex;
+	  align-items: center;
+	  color: #F2A950;
+	  text-transform: uppercase;
+	  visibility: hidden;
+  	}
 
 	@media only screen and (max-width: 600px) {
-			font-size:1em;
-			span {
-				margin-bottom: 20px;
-			}
+		font-size: 1em;
+		span {
+			margin-bottom: 20px;
+		}
 	}
 `
 //Botão redondo de criar relatórios.
 export const ButtonCreateReport = styled.button`
-  background-color:#232f40;
-  width:70px;
-  height:70px;
-  font-size:3em;
-  border-radius:100%;
-  color:#f7f6ee;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-	margin-top: 10px;
-	cursor: pointer;
+background-color: #F2A950;
+width: 100px;
+height: 100px;
+font-size: 5em;
+border-radius: 100%;
+border: 1px solid #F2A950;
+opacity: .7;
+color: #F7F6EE;
+display: flex;
+-webkit-box-pack: center;
+justify-content: center;
+-webkit-box-align: center;
+align-items: center;
+cursor: pointer;
+
+	&:hover {
+		opacity: 1;
+	}
+
 
 	@media only screen and (max-width: 600px) {
-	  width:40px;
-		height:40px;
-		font-size:1.5em;
+	  width: 40px;
+	  height: 40px;
+	  font-size: 1.5em;
 	}
 `
 
@@ -234,16 +253,21 @@ export const ShowReportStyle = styled.div`
 
 export const ButtonSaveReport = styled.div`
 	display:flex;
-	margin-bottom:100px;
+	margin-top:100px;
 
 	button{
-		margin:20px;
-		background-color:#232f40;
-		color:#f7f6ee;
-		width: 100px;
-		height:50px;
-		border-radius:25px;
-		border:none;
+		background-color: #F2A950;
+    	width: 15vw;
+		height: 7.5vh;
+		border: none;
+		margin: 0px 40px;
+		border-radius: 50px;
+		color: #f7f6ee;
+		opacity: 0.8;
+		cursor: pointer;
+		transition: .5s all;
+		font-size: 2.3em;
+		font-weight: bold;
 	}
 `
 
